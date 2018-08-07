0c9e0754e8c81b1a2cf6667967fb13cbbe9e948b
Matheus Reis <mattreisro@gmail.com>
2018-08-06 22:59:39 -0300

    Delete all for new

--- resources/[VRP]/vrp/client/identity.lua
+++
@@ -1,9 +0,0 @@
-local registration_number = cfg.registration_number
-
-function tvRP.setRegistrationNumber(registration)
-  registration_number = registration
-end
-
-function tvRP.getRegistrationNumber()
-  return registration_number
-end
